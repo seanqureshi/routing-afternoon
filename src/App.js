3fe0649bfbb46a071a2820e6f48e374594a3819d
seanqureshi <sean.m.qureshi@gmail.com>
2017-10-10 15:33:38 -0600

    filled out

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import React, { Component } from 'react';
+import router from './router';
+import { Link } from 'react-router-dom'
 
 export default class App extends Component {
 
     render() {
         return (
-           <div>
-               <nav className='nav'>
-                   <div>WestSide University</div> 
-                   <div className='link-wrap'>
-                        <div className='links'>Home</div>
-                        <div className='links'>About</div> 
-                   </div>  
-               </nav>
-            
-           </div> 
+            <div>
+                <nav className='nav'>
+                    <div>WestSide University</div>
+                    <div className='link-wrap'>
+                        <Link to='/' className='links'>Home</Link>
+                        <Link to='/about' className='links'>About</Link>
+                    </div>
+                </nav>
+                {router}
+            </div>
         )
     }
 
--- src/components/ClassList/ClassList.js
+++ src/components/ClassList/ClassList.js
@@ -1,17 +1,41 @@
 import React, { Component } from 'react';
+import axios from 'axios'
+import { Link } from 'react-router-dom'
 
 export default class ClassList extends Component {
   constructor() {
-    super()
+    super();
+
+    this.state = {
+      students: []
+    }
     
   }
 
+  componentDidMount() {
+    return axios.get(`http://localhost:3005/students?class=${this.props.match.params.class}`)
+      .then(response => {
+        this.setState({
+          students: response.data
+        })
+      }
+  )
+}
+
+
+
   render() {
+    let students = this.state.students.map((e,i) => (
+      <Link to={`/student/${e.id}`} key={i}>
+      <h3 >{ e.first_name } { e.last_name }</h3>
+      </Link>
+    ))
+
     return (
       <div className="box">
         <h1></h1>
         <h2>ClassList:</h2>
-
+        {students}
       </div>
     )
   }
